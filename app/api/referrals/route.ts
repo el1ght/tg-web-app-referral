a7651b0fe2ce7e4b51614fd552286124e305cbe0
el1ght <el1ghtifyoucan@gmail.com>
2024-09-04 23:34:02 +0300

    init

---
+++ app/api/referrals/route.ts
@@ -0,0 +1,26 @@
+import {NextRequest, NextResponse} from "next/server";
+import {getReferrals, getReferrer, saveReferral} from "@/lib/storage";
+
+export async function POST(request: NextRequest) {
+    const {userId, referrerId} = await request.json();
+
+    if (!userId || !referrerId) {
+        return NextResponse.json({ error: 'Missing userId or referrerId' }, {status: 400});
+    }
+
+    saveReferral(userId, referrerId);
+    return NextResponse.json({success: true});
+}
+
+export async function GET(request: NextRequest) {
+    const userId = request.nextUrl.searchParams.get('userId');
+
+    if(!userId) {
+        return NextResponse.json({error: 'Missing userId'}, {status: 400});
+    }
+
+    const referrals = getReferrals(userId);
+    const referrer = getReferrer(userId);
+
+    return NextResponse.json({referrals, referrer})
+}
