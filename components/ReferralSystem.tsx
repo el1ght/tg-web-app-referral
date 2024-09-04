505b516afda7807c4993b8353a9d780dccd284eb
el1ght <el1ghtifyoucan@gmail.com>
2024-09-04 23:43:28 +0300

    fix link

--- components/ReferralSystem.tsx
+++ components/ReferralSystem.tsx
@@ -10,7 +10,7 @@ interface ReferralSystemProps {
 const ReferralSystem: React.FC<ReferralSystemProps> = ({initData, userId, startParam}) => {
     const [referrals, setReferrals] = useState<string[]>([])
     const [referrer, setReferrer] = useState<string | null>(null)
-    const INVITE_URL = "https://t.me/ArtistonBot/start"
+    const INVITE_URL = "https://t.me/TheChartonBot/start"
 
     useEffect(() => {
         const checkReferral = async () => {
